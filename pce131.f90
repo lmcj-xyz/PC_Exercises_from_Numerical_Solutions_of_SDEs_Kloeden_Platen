ce78c5a2d82d5b90bf8b11a8ecf3bd121907fb6d
lmcj x380 yoga <dev@lmcj.xyz>
2022-09-30 14:12:07 +0100

    pce131.f90, pi to be defined, exponential RV produced

--- pce131.f90
+++ pce131.f90
@@ -1,6 +1,8 @@
 program pce131
   implicit none
 
+  real :: pi
+
   real :: unif_exp_invtr(10**2) ! Uniform RV for exponential distribution with inverse transform
   real :: unif_gauss_bm1(10**2) ! Uniform RV for Gaussian with Box-Muller
   real :: unif_gauss_bm2(10**2)
@@ -8,6 +10,11 @@ program pce131
   real :: unif_gauss_pm2(10**2) 
 
   real :: exp_dist(10**2) ! Exponential RV with inverse transform method
+  real :: lambda ! Parameter lambda for exponential RV
+
+  real :: gauss_bm(10**2) ! Gaussian RV Box-Muller
+
+  real :: gauss_pm(10**2) ! Gaussian RV Polar Marsaglia
 
   call random_seed() ! Create a seed to generate pseudo random numbers
 
@@ -19,6 +26,14 @@ program pce131
   call random_number(unif_gauss_pm2)
 
   ! Exponentially distributed random numbers
-  
+  !!! It suffices to use a uniform RV U~uniform(0,1) instead of 1 - U
+  !!! since their distribution is the same
+  lambda = 0.5
+  exp_dist = -log(unif_exp_invtr)/lambda
+
+  ! Gaussian random numbers Box-Muller
+  gauss_bm = sqrt(-2*log(unif_gauss_bm1))*cos(2*pi*unif_gauss_bm2)
+
+  ! Gaussian random numbers Polar Marsaglia
 
 end program pce131
